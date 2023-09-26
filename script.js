eb03b1aa9a7814e1f80ba3f3fe4ab836d2182ba0
zakpdev <135481838+zakpdev@users.noreply.github.com>
2023-09-26 08:53:27 -0300

    Create variables for numbers and operator

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ function multiplication(multiplier, multiplicand){
 function division(dividend, divisor){
   return dividend / divisor;
 };
+
+/* A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. 
+Create three variables for each of the parts of a calculator operation. 
+Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later. */
+
+let firstNumber
+let secondNumber
+let operator
+
