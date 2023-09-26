ce82fa3b70586db92a3f406a2ee17bce571cadd1
zakpdev <135481838+zakpdev@users.noreply.github.com>
2023-09-26 08:43:26 -0300

    Create functions for basic arithmetic operations

---
+++ script.js
@@ -0,0 +1,16 @@
+// Functions for basic arithmetic operations
+function addition(addend1, addend2){
+  return a + b;
+};
+
+function subtraction(minuend, subtrahend){
+  return minuend - subtrahend;
+};
+
+function multiplication(multiplier, multiplicand){
+  return multiplier * multiplicand;
+};
+
+function division(dividend, divisor){
+  return dividend / divisor;
+};
